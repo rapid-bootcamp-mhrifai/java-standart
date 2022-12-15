4659eceadffc794f304cac9723c3e4783b014ea4
mhrifai <mkhrifai99@gmail.com>
2022-12-15 09:40:58 +0700

    string joiner

--- src/stringClass/MainStringJoiner.java
+++ src/stringClass/MainStringJoiner.java
@@ -1,4 +1,20 @@
 package stringClass;
 
+import java.util.StringJoiner;
+
 public class MainStringJoiner {
+    public static void main(String[] args) {
+        String[] names = {"Muhamad", "Rifai", "Abdullah"};
+        StringJoiner joiner = new StringJoiner(" - ","{","}");
+
+        System.out.println("Sebelum add:");
+        System.out.println(joiner.toString());
+
+        for (String name: names){
+            joiner.add(name);
+        }
+
+        System.out.println("Setelah di Add:");
+        System.out.println(joiner.toString());
+    }
 }
